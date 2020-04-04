a8c2f9062653e0920129b90d1cc1ac414760db67
Gabe Abrams <gabeabrams@gmail.com>
2020-04-03 21:13:40 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # zaccl
+
 The Zoom App Complete Connection Library, a project that handles everything required to build a zoom-integrated app.
+
+## API Usage
+
+To send a request to the Zoom API (v2), you need to have a developer key and secret. You'll use those when making calls to the API.
+
+### 1. Import
+
+```js
+const visitZoomEndpoint = require('zaccl/api');
+
+// or using es6 imports
+
+import visitZoomEndpoint from 'zaccl/api';
+```
+
+### 2. Visit an endpoint
+
+Arguments:
+
+| Name | Type | Description | Default/Required |
+| :--- | :--- | :--- | :--- |
+| path | string | path of the url to hit | Required |
+| key | string | developer key | Required |
+| secret | string | developer secret | Required |
+| method | string | https request type | GET |
+| params | object | request parameters | none |
+
+Example:
+
+```js
+// Create a meeting
+const meetings = await visitZoomEndpoint({
+  path: '/users/508329/meetings',
+  method: 'POST',
+  key: '38al...',
+  secret: 'fj23...',
+  params: {
+    topic: 'My Zoom Room',
+    start_time: '2021-05-12T11:50:15Z',
+    ...
+  },
+});
+```
---
+++ api.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const jwt = require('jsonwebtoken');
+
+const sendRequest = require('./helpers/sendRequest');
+
+/* -------------------------- Constants ------------------------- */
+
+// Constants
+const EXPIRY_SEC = 60;
+
+/* ------------------------- Throttlers ------------------------- */
+
+let throttleReady = Promise.resolve();
+const resetThrottle = () => {
+  throttleReady = new Promise((r) => {
+    setTimeout(r, 1000);
+  });
+};
+
+/* -------------------------- Function -------------------------- */
+
+/**
+ * Send an API request to Zoom
+ * @param {string} path - the url path to hit
+ * @param {string} key - the Zoom API key to use to generate credentials
+ * @param {string} secret - the Zoom API secret to use to generate credentials
+ * @param {string} [method=GET] - the https method to use
+ * @param {object} [params] - the request params to include
+ */
+module.exports = async (opts) => {
+  const {
+    method,
+    params,
+    key,
+    secret,
+  } = opts;
+
+  /* --------------------- Deal with Throttle --------------------- */
+  await throttleReady;
+  resetThrottle();
+
+  /* ---------------------- Generate a Token ---------------------- */
+  const token = jwt.sign({
+    iss: key,
+    exp: (Date.now() + (EXPIRY_SEC * 1000)),
+  }, secret);
+
+  /* ---------------------- Send the Request ---------------------- */
+  return sendRequest({
+    params,
+    method,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    host: 'api.zoom.us',
+    path: path.join('/v2', opts.path),
+  })
+    .then((response) => {
+      console.log(response.status);
+      return response.body;
+    });
+};
