8040e45c24360db493c2569d92d98cded2ebb85d
Grace Whitney <g_whitney@college.harvard.edu>
2020-05-06 11:31:36 -0400

    First API test skeleton

--- test/API/index.spec.js
+++ test/API/index.spec.js
@@ -1,1 +1,41 @@
-// TODO: add mocha-based tests
+import assert from 'assert';
+
+// import zoom request stub
+const stubZoomRequest = require('./helpers/stubZoomRequest');
+
+// import class to test
+const api = require('../../api');
+
+describe('API', function () {
+  describe('API > addRule', function () {
+    it('Converts a path template to the correct regexp', function () {
+
+    });
+
+    it('Updates the rule map', function () {
+
+    });
+
+    it('Allows overwriting a rule', function () {
+
+    });
+  });
+
+  describe('API > visitEndpoint', function () {
+    it('Looks for a matching throttle rule', function () {
+
+    });
+
+    it('Visits the correct endpoint and method', function () {
+
+    });
+
+    it('Maintains a daily count per endpoint', function () {
+
+    });
+
+    it('Throttles per endpoint', function () {
+
+    }); 
+  });
+});
