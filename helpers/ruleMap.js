ad9c5b4161f83ec333d73657fda4f23c86ee1288
Grace Whitney <g_whitney@college.harvard.edu>
2020-05-25 10:48:26 -0400

    added updateDailyReset method to ruleMap

--- helpers/ruleMap.js
+++ helpers/ruleMap.js
@@ -4,25 +4,31 @@ const { default: PQueue } = require('p-queue');
  * Class for storing throttle and daily limit rules
  * @author Grace Whitney
  */
-
 class RuleMap {
+  /**
+   * Create a new RuleMap instance
+   * @author Grace Whitney
+   */
   constructor() {
     // Stores a map from method -> (map from path regex -> rule object)
-    this.map = new Map();
-    // Default unthrottled queue for non-rate-limited endpoints
-    this.unthrottled = new PQueue();
+    this.map = {};
+    // Default unthrottled rule for non-rate-limited endpoints
+    this.default = {
+      queue: new PQueue(),
+    };
   }
 
   /**
-   * Add a new rule to the map
+   * Add a new rule to the map.
    * @author Grace Whitney
-   * @param {object} regexp - regexp object of endpoint path template
-   * @param {string} method - method of endpoint
-   * @param [maxRequestsPerInterval=unlimited] - the maximum number of
+   * @param {object} opts - object containing all options
+   * @param {string} opts.regexp - regexp string of endpoint path template
+   * @param {string} opts.method - method of endpoint
+   * @param [opts.maxRequestsPerInterval=unlimited] - the maximum number of
    *   requests allowed each rate limit interval
-   * @param {number} [millisecondsPerInterval=1000] - the milliseconds per
+   * @param {number} [opts.millisecondsPerInterval=1000] - the milliseconds per
    *   rate interval
-   * @param {number} [maxRequestsPerDay=unlimited] - the maximum number of
+   * @param {number} [opts.maxRequestsPerDay=unlimited] - the maximum number of
    *   requests allowed each day
    * @returns
    */
@@ -37,40 +43,42 @@ class RuleMap {
     // case insensitive by method
     const method = opts.method.toUpperCase();
 
-    // create new throttler if endpoint is rate limited,
-    //  otherwise use the default queue
-    const queue = maxRequestsPerInterval
-      ? new PQueue({
-        intervalCap: maxRequestsPerInterval,
-        interval: millisecondsPerInterval,
-      })
-      : this.unthrottled;
+    // instantiate new throttler with specified limits
+    const queue = new PQueue({
+      intervalCap: maxRequestsPerInterval,
+      interval: millisecondsPerInterval || 1000,
+    });
 
-    // Rule object holds queue, daily tokens, and timestamp of last lookup
+    // rule object
     const rule = {
+      // throttled queue
       queue,
+      // total allowed calls per day
       maxRequestsPerDay,
+      // remaining allowed calls today
       dailyTokensRemaining: maxRequestsPerDay,
-      timeLastAccessed: Date.now(),
+      // timestamp after which to reset tokens to full,
+      //   initially set to next UTC midnight
+      resetAfter: (new Date()).setUTCHours(24, 0, 0, 0),
     };
 
     // add rule to nested map of the method, with the path regexp as its key
     let innerMap;
-    if (this.map.has(method)) {
-      innerMap = this.map.get(method);
+    if (method in this.map) {
+      innerMap = this.map[method];
     } else {
-      innerMap = new Map();
-      this.map.set(method, innerMap);
+      innerMap = {};
+      this.map[method] = innerMap;
     }
 
     // throw an error on duplicate rule
-    if (innerMap.has(regexp)) {
+    if (regexp in innerMap) {
       throw new Error(
         'A rule for this path already exists. You may not define duplicate rules.'
       );
     }
     // otherwise, add rule to map
-    innerMap.set(regexp, rule);
+    innerMap[regexp] = rule;
   }
 
   /**
@@ -80,34 +88,29 @@ class RuleMap {
    * @param {string} method - method of endpoint to look up
    * @param {string} path - path of endpoint to look up
    * @returns {object} rule object for given endpoint if it exists,
-   *    or default unthrottled rule
+   *    or default unthrottled rule, in the form:
+   *    {queue, maxRequestsPerDay?, dailyTokensRemaining?, resetAfter?}
    */
   lookup(method, path) {
-    // default rule to return if no match is found
-    const defaultRule = {
-      queue: this.unthrottled,
-      maxRequestsPerDay: null,
-      dailyTokensRemaining: null,
-      timeLastAccessed: Date.now(),
-    };
-
     // if the method is not in the map, return default directly
     if (!this.map.has(method)) {
-      return defaultRule;
+      return this.default;
     }
 
     // iterate through method map keys, looking for regexp match
-    const innerMap = this.map.get(method);
+    const innerMap = this.map[method];
     innerMap.keys().forEach((regexp) => {
+      // convert regexp string to RegExp object
+      const re = new RegExp(regexp);
       // on match, update and return rule object
-      if (regexp.test(path)) {
-        const rule = innerMap.get(regexp);
+      if (re.test(path)) {
+        const rule = innerMap[regexp];
         // perform daily counter arithmetic if necessary
         if (rule.maxRequestsPerDay) {
           // check if we need to reset daily counter
-          const midnight = (new Date()).setHours(0, 0, 0, 0);
-          if (rule.timeLastAccessed < midnight) {
+          if (rule.timeLastAccessed < rule.resetAfter) {
             rule.dailyTokensRemaining = rule.maxRequestsPerDay;
+            rule.resetAfter = (new Date()).setUTCHours(24, 0, 0, 0);
           }
           rule.timeLastAccessed = Date.now();
         }
@@ -120,6 +123,35 @@ class RuleMap {
         return returnedRule;
       }
     });
+
+    // if no match, return default
+    return this.default;
+  }
+
+  /**
+   * Change the resetAfter attribute of an existing rule object, in case of
+   *    unexpected daily rate limit error
+   * @author Grace Whitney
+   * @param {object} opts - object containing all options
+   * @param {object} opts.regexp - regexp object of endpoint path template
+   * @param {string} opts.method - method of endpoint
+   * @param {object} opts.resetAfter - Date object representing new reset time
+   */
+  updateDailyReset(opts) {
+    const {
+      regexp,
+      method,
+      resetAfter,
+    } = opts;
+
+    if (this.map.has(method) && this.map.get(method).has(regexp)) {
+      this.map.get(method).get(regexp).resetAfter = resetAfter;
+    } else {
+      // TODO: decide whether to add daily limit to default queue
+      throw new Error(
+        `The rule for path ${regexp.toString()} and method ${method} does not exist and cannot be updated.`
+      );
+    }
   }
 }
 
