701574a8c17f5877d94bc4cd782b3516fd9d38f8
apandey00 <aryanpnd74@gmail.com>
2020-06-01 13:08:45 -0400

    Function docs for Meeting

--- api/endpoints/Meeting.js
+++ api/endpoints/Meeting.js
@@ -22,8 +22,9 @@ class Meeting extends EndpointCategory {
  * @instance
  * @memberof api.meeting
  * @method get
- * @param {number} meetingId - the Zoom id of the meeting
- * @param {string} occurrenceId - the id for the meeting occurrence
+ * @param {number} meetingId - the Zoom id of the meeting (int64 format)
+ * @param {string} [occurrenceId] - the id for the meeting occurrence
+ * @param {boolean} [showAllOccurrences] - set to true to get meeting details of all past occurrences
  * @return {Meeting} Zoom meeting object {@link https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meeting#responses}
  */
 Meeting.get = function (options) {
@@ -46,4 +47,73 @@ Meeting.get.requiredParams = ['meetingId'];
 Meeting.get.scopes = [
   'meeting:read:admin',
   'meeting:read',
+];
+
+/**
+ * Create a new meeting
+ * @author Aryan Pandey
+ * @async
+ * @instance
+ * @memberof api.meeting
+ * @method create
+ * @param {string} userId - the user id or email address of the user
+ * @param {Meeting} meetingObj - Zoom meeting object with details of meeting to be created
+ * @return {Meeting} Zoom meeting object {@link https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingcreate#request-body}}
+ */
+Meeting.create = function (options) {
+
+  // TODO: write core function
+
+};
+Meeting.create.action = 'create a new meeting';
+Meeting.create.requiredParams = ['userId, meetingObj'];
+Meeting.create.scopes = [
+  'meeting:write:admin',
+  'meeting:write',
+];
+
+/**
+ * Update a meeting
+ * @author Aryan Pandey
+ * @async
+ * @instance
+ * @memberof api.meeting
+ * @method update
+ * @param {number} meetingId - The meeting id (int64 format)
+ * @param {Meeting} meetingObj - Zoom meeting object with updated details {@link https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingupdate#request-body}
+ * @param {string} [occurrenceId] - the id for the meeting occurrence
+ */
+Meeting.update = function (options) {
+
+  // TODO: write core function
+
+};
+Meeting.update.action = 'update the details of a meeting';
+Meeting.update.requiredParams = ['meetingId, meetingObj'];
+Meeting.update.scopes = [
+  'meeting:write:admin',
+  'meeting:write',
+];
+
+/**
+ * Delete a meeting
+ * @author Aryan Pandey
+ * @async
+ * @instance
+ * @memberof api.meeting
+ * @method delete
+ * @param {number} meetingId - The meeting id (int64 format)
+ * @param {string} occurrenceId - the id for the meeting occurrence
+ * @param {boolean} notifyHosts - Sends email about cancellation to hosts when true
+ */
+Meeting.delete = function (options) {
+
+  // TODO: write core function
+
+};
+Meeting.delete.action = 'delete a meeting';
+Meeting.delete.requiredParams = ['meetingId'];
+Meeting.delete.scopes = [
+  'meeting:write:admin',
+  'meeting:write',
 ];
