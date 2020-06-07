d0492cdb5be7eb8eea2de8547cdf6cd4d1659376
apandey00 <aryanpnd74@gmail.com>
2020-06-07 00:45:13 -0400

    double encode functionality

--- EndpointCategory/helpers/utils.js
+++ EndpointCategory/helpers/utils.js
@@ -6,4 +6,20 @@ const ERROR_CODES = require('../../ERROR_CODES');
 
 module.exports = {
 
+  /**
+   * Checks argument string to see if it needs to be encoded
+   *   Double encodes and returns if needed
+   *   Otherwise returns original string
+   *   Function assumes valid string is passed
+   * @author Aryan Pandey
+   * @param {String} str - String that might need to be double encoded
+   * @returns {String} - processed string
+   */
+  doubleEncodeIfNeeded: (str) => {
+  // Variable to decide whether to double encode str
+    const encodingRequired = str.startsWith('/') || str.includes('//');
+
+    return (encodingRequired ? encodeURIComponent(encodeURIComponent(str))
+      : str);
+  },
 };
