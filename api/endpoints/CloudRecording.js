ef0843fa6f00870b4a569e428539ad739105f76b
apandey00 <aryanpnd74@gmail.com>
2020-06-07 19:23:35 -0400

    docs for recording/get list

--- api/endpoints/CloudRecording.js
+++ api/endpoints/CloudRecording.js
@@ -25,7 +25,7 @@ class CloudRecording extends EndpointCategory {
  * @memberof api.cloudRecording
  * @method list
  * @param {object} options - object containing all arguments
- * @param {String} options.meetingId - the Zoom ID of the meeting
+ * @param {string} options.meetingId - the Zoom meeting ID or UUID
  * @return {Recording} Zoom meeting recording object {@link https://marketplace.zoom.us/docs/api-reference/zoom-api/cloud-recording/recordingget#responses}
  */
 CloudRecording.list = function (options) {
@@ -38,6 +38,7 @@ CloudRecording.list = function (options) {
   }
 
   return this.visitEndpoint({
+    // Call function on meetingId to handle double encoding if necessary
     path: `/meetings/${utils.doubleEncodeIfNeeded(options.meetingId)}/recordings`,
     method: 'GET',
     errorMap: {
@@ -58,6 +59,47 @@ CloudRecording.list = function (options) {
 CloudRecording.list.action = 'get all recordings of a meeting';
 CloudRecording.list.requiredParams = ['meetingId'];
 CloudRecording.list.scopes = [
+  'recording:read:admin',
+  'recording:read',
+];
+
+/**
+ * List all cloud recordings of a user
+ * @author Aryan Pandey
+ * @async
+ * @instance
+ * @memberof api.cloudRecording
+ * @method getList
+ * @param {object} options - object containing all arguments
+ * @param {string} options.userId - the user ID or email address of the user
+ * @param {number} [options.pageSize=300] - number of records
+ *   returned from a single API call
+ * @param {string} [options.nextPageToken=null] - token used to pageinate
+ *   through large result sets
+ *
+ * // Note: Confusing -- look further into this
+ * @param {string} [options.queryMetadata=false] -
+ *
+ * @param {boolean} [options.searchTrash=false] - if truthy,
+ *   include recordings from trash
+ * @param {string} [options.trashtype='meeting_recordings'] - Indicate type of
+ *   cloud recording to retreive from the trash.
+ *   Options - meeting_recordings or recording_file
+ * @param {string} [options.startDate=null] - query start date in
+ *   'yyyy-mm-dd' format within last 6 months
+ * @param {string} [options.endDate=null] - query end date in
+ *   'yyyy-mm-dd' format within last 6 months
+ * @return {RecordingList} List of Zoom Recordings {@link https://marketplace.zoom.us/docs/api-reference/zoom-api/cloud-recording/recordingslist#responses}
+ */
+CloudRecording.getList = function (options) {
+  // Note: the default for page_size on zoom is 30 with max of 300.
+
+  // TODO:  Write core functions
+
+};
+CloudRecording.getList.action = 'list all cloud recordings of a user';
+CloudRecording.getList.requiredParams = ['userId'];
+CloudRecording.getList.scopes = [
   'recording:read:admin',
   'recording:read',
 ];
