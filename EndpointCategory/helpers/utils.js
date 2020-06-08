742b9261e91e9300acba4dd70788e0558452a421
apandey00 <aryanpnd74@gmail.com>
2020-06-08 14:48:05 -0400

    date regex helper

--- EndpointCategory/helpers/utils.js
+++ EndpointCategory/helpers/utils.js
@@ -12,8 +12,8 @@ module.exports = {
    *   Otherwise returns original string
    *   Function assumes valid string is passed
    * @author Aryan Pandey
-   * @param {String} str - String that might need to be double encoded
-   * @returns {String} - processed string
+   * @param {string} str - String that might need to be double encoded
+   * @returns {string} - processed string
    */
   doubleEncodeIfNeeded: (str) => {
   // Variable to decide whether to double encode str
@@ -21,5 +21,16 @@ module.exports = {
 
     return (encodingRequired ? encodeURIComponent(encodeURIComponent(str))
       : str);
+  },
+
+  /**
+   * Returns true if string is in the format yyyy-mm-dd
+   * @author Aryan Pandey
+   * @param {string} date - date string that needs to be validated
+   * @returns {boolean} true if valid, false otherwise
+   */
+  validateDate: (date) => {
+    const regex = /\d{4}-([1-9]|0[1-9]|1[012])-([1-9]|0[1-9]|[12][0-9]|3[01])$/;
+    return regex.test(date);
   },
 };
