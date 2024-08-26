e7909802d9cb71387ef1c828ff05471fd14661fc
Yuen Ler Chow <lerchow@gmail.com>
2024-08-26 09:34:28 -0700

    trim each answer choice

--- src/endpoints/ECatMeeting.ts
+++ src/endpoints/ECatMeeting.ts
@@ -506,7 +506,12 @@ class ECatMeeting extends EndpointCategory {
           // Set of fields that are common to all question types
           const commonFields = {
             answerRequired: !!answerRequired,
-            answers,
+            // trim each answer
+            answers: answers 
+              ? answers.map((answer: string) => {
+                return answer.trim();
+              }) 
+              : [],
             name,
             rightAnswers,
           };
